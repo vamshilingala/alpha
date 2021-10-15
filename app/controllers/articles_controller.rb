8d26bb910b5f09c5c1cee990860c936aba59d5d6
vamshilingala <lingalavamshi050@gmail.com>
2021-10-15 15:24:57 +0530

    all done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 def show
-    @article=Article.find(params[:id])
+    #@article=Article.find(params[:id])
 
 end
 def index
@@ -10,13 +11,14 @@ def new
     @article=Article.new
 end
 def edit
-    @article=Article.find(params[:id])
+    #@article=Article.find(params[:id])
 
 end
 
 def create
-   @article=Article.new(params.require(:article).permit(:title, :description)) 
-    if @article.save
+   #@article=Article.new(params.require(:article).permit(:title, :description)) 
+   @article=Article.new(article_params) 
+   if @article.save
         flash[:notice] = "Article was created successfully."
     redirect_to @article
     else
@@ -24,8 +26,9 @@ def create
     end
 end
 def update
-    @article=Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    #@article=Article.find(params[:id])
+    #if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
         flash[:notice]="Article was updated successfully"
         redirect_to @article
 else
@@ -33,12 +36,15 @@ else
 end
 end
 def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+end 
+private
+def set_article 
+    @article=Article.find(params[:id])
 end
-def home
-end
-def about
+def article_params
+    params.require(:article).permit(:title,:description)
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles #, only:[:show,:index, :new, :create, :edit, :update, :destroy]
-  #root 'pages#home'
+  root 'pages#home'
   #get 'about', to :'pages#about'
 end

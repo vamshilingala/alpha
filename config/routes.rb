0cdf66398c689bbd81f3dc7b3cfa6c2259b5397f
vamshilingala <lingalavamshi050@gmail.com>
2021-10-18 12:22:44 +0530

    add user from backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :articles #, only:[:show,:index, :new, :create, :edit, :update, :destroy]
   root 'pages#home'
   #get 'about', to :'pages#about'
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end

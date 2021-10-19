190fe3dd308ac2bb4f873ad90c1384a266d96a8c
vamshilingala <lingalavamshi050@gmail.com>
2021-10-19 17:16:04 +0530

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles #, only:[:show,:index, :new, :create, :edit, :update, :destroy]
   root 'pages#home'
-  #get 'about', to :'pages#about'
+  get 'about', to: 'pages#about'
+  resources :articles
   get 'signup', to: 'users#new'
-  #post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
